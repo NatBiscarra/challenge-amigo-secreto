8312ee3d75d11ce8d6ad2a0fd46555f57bfcad68
NatBiscarra <natbiscarra@gmail.com>
2025-01-31 18:29:50 -0300

    Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaAmigos = []
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value; //Captura o valor de entrada (nome) inserido pelo usuário
+    
+    if (nomeAmigo == '' || nomeAmigo == ' ') {
+     alert('Por favor, insira um nome.');
+     return false;     
+    } else {
+        listaAmigos.push(nomeAmigo); //Se não está vazio, o nome inserido é add a lista 
+        let lista = document.getElementById('listaAmigos');
+        for (i = 0; i < listaAmigos.length; ++i) {
+            let li = document.createElement('li');
+            li.innerText = listaAmigos[i];
+            lista.appendChild(li);
+        }
+        limparCampo();        
+    } 
+
+}
+
+
+//limpar campo do input depois de add nome do amigo
+function limparCampo() {
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = ''; 
+}
+
+//limpar lista abaixo do campo para não haver duplicação de listas
+function limparLista() {
+    lista = document.getElementById('listaAmigos'); //vai retornar a lista de amigos?
+    lista.innerHTML = '';
+}
